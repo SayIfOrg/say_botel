77268c7d9c69ed7fca78a51490620dd201a886dd
AmirW <eng.amir.bu@gmail.com>
2022-08-27 17:26:51 +0430

    change improper grpc names

--- grpc_gate/server.py
+++ grpc_gate/server.py
@@ -3,31 +3,25 @@ import logging
 import grpc
 
 
-from . import helloworld_pb2
-from . import helloworld_pb2_grpc
+from . import webpage_pb2
+from . import webpage_pb2_grpc
 
 from main import bot
 from utils.normalize import clean_html
 
 
-class MYServicer(helloworld_pb2_grpc.GreeterServicer):
-    async def SayHello(self, request, context):
-        response = helloworld_pb2.HelloReply(message="Hello, %s!" % request.name)
-        await bot.send_message(chat_id=73789947, text=request.name)
-        return response
-
+class PublishServicer(webpage_pb2_grpc.PublishServicer):
     async def PublishRichText(self, request, context):
-        response = helloworld_pb2.HelloReply(message="Hello, %s!" % "OK")
         message = clean_html(request.body)
-        await bot.send_message(chat_id=73789947, text=message, parse_mode="html")
-        return response
+        res = await bot.send_message(chat_id=73789947, text=message, parse_mode="html")
+        return webpage_pb2.Result(message="OK")
 
 
 async def serve():
     logging.basicConfig()
     # server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     server = grpc.aio.server()
-    helloworld_pb2_grpc.add_GreeterServicer_to_server(MYServicer(), server)
+    webpage_pb2_grpc.add_PublishServicer_to_server(PublishServicer(), server)
     server.add_insecure_port("[::]:5060")
     # server.start()
     await server.start()
