3605c52c6b2165800ebdbc5b50826d9ff287df82
AmirW <eng.amir.bu@gmail.com>
2023-04-10 18:24:23 +0330

    async call in main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def configure():
     load_dotenv(dotenv_path=".env", override=True)
 
 
-if __name__ == "__main__":
+async def amain():
     configure()
     import handlers
     from db import models
@@ -22,17 +22,21 @@ if __name__ == "__main__":
 
     if sys.argv[1] == "grpc":
         # grpc_gate.server.serve()
-        asyncio.run(grpc_gate.server.serve(db_initializer=get_session, bot=bot))
+        await grpc_gate.server.serve(db_initializer=get_session, bot=bot)
     elif sys.argv[1] == "poll":
-        bot.message_handler(commands=["register_me"])(handlers.register_for_blog(db_initializer=get_session))
+        bot.message_handler(commands=["register_me"])(
+            handlers.register_for_blog(db_initializer=get_session)
+        )
 
         # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
         bot.message_handler(func=lambda message: True)(handlers.echo_message)
 
-        asyncio.run(bot.polling(request_timeout=1000))
+        await bot.polling(request_timeout=1000)
     elif sys.argv[1] == "dbpush":
-        async def db_create_all():
-            async with engine.begin() as conn:
-                await conn.run_sync(models.Base.metadata.drop_all)
-                await conn.run_sync(models.Base.metadata.create_all)
-        asyncio.run(db_create_all())
+        async with engine.begin() as conn:
+            await conn.run_sync(models.Base.metadata.drop_all)
+            await conn.run_sync(models.Base.metadata.create_all)
+
+
+if __name__ == "__main__":
+    asyncio.run(amain())
