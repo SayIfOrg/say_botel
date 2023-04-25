fe815a0d0195515a96430ffc1172f9c24afecc0e
AmirW <eng.amir.bu@gmail.com>
2023-04-25 18:45:16 +0330

    refactor handlers registration to the handlers.py

--- botel/handlers.py
+++ botel/handlers.py
@@ -21,10 +21,10 @@ from botel.db.operations.create import register_instance
 from botel.grpc_gate import webpage_pb2_grpc, webpage_pb2
 
 
-def provide_with_db(func):
+def provide_with_db(db_initializer: Callable[[], AsyncGenerator[AsyncSession, None]]):
     """Inject AsyncSession to bot handlers"""
 
-    def get_injects(db_initializer: Callable[[], AsyncGenerator[AsyncSession, None]]):
+    def get_injects(func):
         # Don't use wraps, changing the func signature
         async def wrapper(message, data, bot):
             async with db_initializer() as db:
@@ -39,6 +39,7 @@ class QUERY_DATA(str, Enum):
     """
     The constants that query callbacks use
     """
+
     REGISTER_CHANEL = "add_a_channel"
     CANCEL = "cancel"
 
@@ -47,6 +48,7 @@ class STATES(str, Enum):
     """
     The possible states
     """
+
     REGISTERING_CHANEL = "add_a_channel"
 
 
@@ -58,43 +60,66 @@ markup.add(itembtn1)
 MENUE_MARKUP = markup
 
 
-@provide_with_db
-async def register_for_blog(db: AsyncSession, message, data, bot):
-    register_token = message.text.split(" ")[1]
-    async with grpc.aio.insecure_channel("localhost:5061") as channel:
-        stub = webpage_pb2_grpc.ManageInstanceStub(channel)
-        response = await stub.ValidateToken(
-            webpage_pb2.Token(token=register_token, commit=False)
-        )
-        if response.name:
+def register_handlers(
+    telebot: AsyncTeleBot,
+    db_initializer: Callable[[], AsyncGenerator[AsyncSession, None]],
+):
+    # Private
+    @telebot.message_handler(commands=["register_me"])
+    @provide_with_db(db_initializer)
+    async def register_for_blog(db: AsyncSession, message, data, bot):
+        register_token = message.text.split(" ")[1]
+        async with grpc.aio.insecure_channel("localhost:5061") as channel:
+            stub = webpage_pb2_grpc.ManageInstanceStub(channel)
             response = await stub.ValidateToken(
-                webpage_pb2.Token(token=register_token, commit=True)
+                webpage_pb2.Token(token=register_token, commit=False)
             )
             if response.name:
-                _ = await register_instance(db, response.id, 1, message.chat)
-            await bot.reply_to(message, "succeeded" if response.name else "Error")
-            return
-        await bot.reply_to(message, "It's not valid")
+                response = await stub.ValidateToken(
+                    webpage_pb2.Token(token=register_token, commit=True)
+                )
+                if response.name:
+                    _ = await register_instance(db, response.id, 1, message.chat)
+                await bot.reply_to(message, "succeeded" if response.name else "Error")
+                return
+            await bot.reply_to(message, "It's not valid")
 
+    @telebot.message_handler(commands=["m"])
+    async def menu(message: Message, data, bot: AsyncTeleBot):
+        """
+        Send the menu inline keyboard
+        """
+        await bot.send_message(
+            message.chat.id, "Choose one option:", reply_markup=MENUE_MARKUP
+        )
 
-async def echo_message(message: Message, data, bot: AsyncTeleBot):
-    await bot.reply_to(message, message.text)
-
-
-async def menu(message: Message, data, bot: AsyncTeleBot):
-    """
-    Send the menu inline keyboard
-    """
-    await bot.send_message(
-        message.chat.id, "Choose one option:", reply_markup=MENUE_MARKUP
+    @telebot.callback_query_handler(
+        func=lambda x: x.data == QUERY_DATA.REGISTER_CHANEL.value
     )
+    async def add_to_a_channel_state(message: CallbackQuery, data, bot: AsyncTeleBot):
+        """
+        Set the user's state on adding a channel and send the appropriate message
+        """
+        markup = InlineKeyboardMarkup()
+        itembtna = InlineKeyboardButton("Cancel", callback_data=QUERY_DATA.CANCEL.value)
+        markup.row(itembtna)
+        t1 = bot.edit_message_text(
+            "Add the bot to your channel and then send us a forwarded message from that channel",
+            chat_id=message.message.chat.id,
+            message_id=message.message.id,
+            reply_markup=markup,
+        )
+        t2 = bot.set_state(message.from_user.id, STATES.REGISTERING_CHANEL.value)
+        await asyncio.gather(t1, t2)
 
-
-async def add_to_a_channel_state(message: CallbackQuery, data, bot: AsyncTeleBot):
-    """
-    Set or Unset the user's state on adding a channel and send the appropriate message
-    """
-    if message.data == QUERY_DATA.CANCEL.value:
+    @telebot.callback_query_handler(
+        func=lambda x: x.data == QUERY_DATA.CANCEL.value,
+        state=STATES.REGISTERING_CHANEL.value,
+    )
+    async def add_to_a_channel_state(message: CallbackQuery, data, bot: AsyncTeleBot):
+        """
+        Unset the user's state on adding a channel and send the appropriate message
+        """
         t1 = bot.delete_state(message.from_user.id)
 
         t2 = bot.edit_message_text(
@@ -104,73 +129,71 @@ async def add_to_a_channel_state(message: CallbackQuery, data, bot: AsyncTeleBot
             reply_markup=MENUE_MARKUP,
         )
         await asyncio.gather(t1, t2)
-        return
-    markup = InlineKeyboardMarkup()
-    itembtna = InlineKeyboardButton("Cancel", callback_data=QUERY_DATA.CANCEL.value)
-    markup.row(itembtna)
-    t1 = bot.edit_message_text(
-        "Add the bot to your channel and then send us a forwarded message from that channel",
-        chat_id=message.message.chat.id,
-        message_id=message.message.id,
-        reply_markup=markup,
-    )
-    t2 = bot.set_state(message.from_user.id, STATES.REGISTERING_CHANEL.value)
-    await asyncio.gather(t1, t2)
 
-
-async def add_to_a_channel(message: Message, data, bot: AsyncTeleBot):
-    """
-    Tries to add the channel that it's message is forwarded
-    """
-    bot_user = await bot.get_me()
-    try:
-        bot_channel_membership = await bot.get_chat_member(
-            message.forward_from_chat.id, bot_user.id
-        )
-    except ApiTelegramException as e:
-        if e.error_code == 403:
-            markup = InlineKeyboardMarkup()
-            itembtn1 = InlineKeyboardButton("try again", callback_data="add_a_channel")
-            markup.add(itembtn1)
-            t1 = bot.send_message(
-                message.chat.id,
-                "you are not added the bot yet. Cancelled",
-                reply_markup=markup,
+    @telebot.message_handler(state=STATES.REGISTERING_CHANEL.value)
+    async def add_to_a_channel(message: Message, data, bot: AsyncTeleBot):
+        """
+        Tries to add the channel that it's message is forwarded
+        """
+        bot_user = await bot.get_me()
+        try:
+            bot_channel_membership = await bot.get_chat_member(
+                message.forward_from_chat.id, bot_user.id
             )
-            t2 = bot.delete_state(message.from_user.id)
-            await asyncio.gather(t1, t2)
-            return
-        raise
-    if bot_channel_membership:
-        channel = await bot.get_chat(message.forward_from_chat.id)
-        # TODO register the channel
-        return
+        except ApiTelegramException as e:
+            if e.error_code == 403:
+                markup = InlineKeyboardMarkup()
+                itembtn1 = InlineKeyboardButton(
+                    "try again", callback_data="add_a_channel"
+                )
+                markup.add(itembtn1)
+                t1 = bot.send_message(
+                    message.chat.id,
+                    "you are not added the bot yet. Cancelled",
+                    reply_markup=markup,
+                )
+                t2 = bot.delete_state(message.from_user.id)
+                await asyncio.gather(t1, t2)
+                return
+            raise
+        if bot_channel_membership:
+            channel = await bot.get_chat(message.forward_from_chat.id)
+            # TODO register the channel
 
+    # Chat
+    @telebot.my_chat_member_handler(func=lambda x: True)
+    async def echo_bot_chat_events(update: ChatMemberUpdated, data, bot: AsyncTeleBot):
+        """
+        Complaines whenever the bot's rights are given away from it
+        Confirms rights are given to the bot
+        """
+        logging.info(str(update.difference))
 
-async def echo_bot_chat_events(update: ChatMemberUpdated, data, bot: AsyncTeleBot):
-    """
-    Complaines whenever the bot's rights are given away from it
-    Confirms rights are given to the bot
-    """
-    logging.info(str(update.difference))
+    @telebot.channel_post_handler(func=lambda x: True)
+    async def channel_post(update: Message, data, bot: AsyncTeleBot):
+        """
+        Prepare the channel post to be commentable
+        """
+        logging.info("channel_post received")
 
+    # # Group
+    @telebot.message_handler(chat_types=["supergroup"])
+    async def group_messages(update: Message, data, bot: AsyncTeleBot):
+        """
+        Handle group messages
+        """
+        logging.info("group_messages received")
 
-async def channel_post(update: Message, data, bot: AsyncTeleBot):
-    """
-    Prepare the channel post to be commentable
-    """
-    logging.info("channel_post received")
+    @telebot.message_handler(
+        func=lambda x: x.reply_to_message, chat_types=["supergroup"]
+    )
+    async def group_replies(update: Message, data, bot: AsyncTeleBot):
+        """
+        Handle group reply messages
+        """
+        logging.info(str(update.difference))
 
-
-async def group_messages(update: Message, data, bot: AsyncTeleBot):
-    """
-    Handle group messages
-    """
-    logging.info("group_messages received")
-
-
-async def group_replies(update: Message, data, bot: AsyncTeleBot):
-    """
-    Handle group reply messages
-    """
-    logging.info(str(update.difference))
+    # # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
+    @telebot.message_handler(func=lambda message: True)
+    async def echo_message(message: Message, data, bot: AsyncTeleBot):
+        await bot.reply_to(message, message.text)
--- main.py
+++ main.py
@@ -8,7 +8,6 @@ from telebot.asyncio_filters import StateFilter
 from telebot.async_telebot import AsyncTeleBot
 
 import botel.grpc_gate.server
-from botel.handlers import STATES, QUERY_DATA
 
 
 def configure():
@@ -17,7 +16,7 @@ def configure():
 
 async def amain():
     configure()
-    from botel import handlers
+    from botel.handlers import register_handlers
     from botel.db import models
     from botel.db.engine import get_session, engine
 
@@ -38,35 +37,7 @@ async def amain():
         for bot in bots:
             telebot = AsyncTeleBot(bot.api_token)
             telebot.add_custom_filter(StateFilter(telebot))
-            # Private
-            telebot.message_handler(commands=["register_me"])(
-                handlers.register_for_blog(db_initializer=get_session)
-            )
-            telebot.message_handler(commands=["m"])(handlers.menu)
-            telebot.callback_query_handler(
-                func=lambda x: x.data == QUERY_DATA.REGISTER_CHANEL.value
-            )(handlers.add_to_a_channel_state)
-            telebot.callback_query_handler(
-                func=lambda x: x.data == QUERY_DATA.CANCEL.value,
-                state=STATES.REGISTERING_CHANEL.value,
-            )(handlers.add_to_a_channel_state)
-            telebot.message_handler(state=STATES.REGISTERING_CHANEL.value)(
-                handlers.add_to_a_channel
-            )
-            # Chat
-            telebot.my_chat_member_handler(func=lambda x: True)(
-                handlers.echo_bot_chat_events
-            )
-            telebot.channel_post_handler(func=lambda x: True)(handlers.channel_post)
-            # Group
-            telebot.message_handler(
-                func=lambda x: x.reply_to_message, chat_types=["supergroup"]
-            )(handlers.group_replies)
-            telebot.message_handler(chat_types=["supergroup"])(handlers.group_messages)
-
-            # Handle all other messages with content_type 'text' (content_types defaults to ['text'])
-            telebot.message_handler(func=lambda message: True)(handlers.echo_message)
-
+            register_handlers(telebot, get_session)
             poll_task = asyncio.create_task(telebot.polling(request_timeout=1000))
             poll_tasks.append(poll_task)
         await asyncio.gather(*poll_tasks)
