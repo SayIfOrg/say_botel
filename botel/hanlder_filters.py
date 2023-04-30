d053633dfd4bee0f1c3ce5839650045d2c2a05be
AmirW <eng.amir.bu@gmail.com>
2023-04-30 12:06:46 +0330

    add a SimpleCustomFilter for commenting

--- botel/hanlder_filters.py
+++ botel/hanlder_filters.py
@@ -1,7 +1,11 @@
+from typing import Callable, AsyncGenerator
+
 from sqlalchemy.ext.asyncio import AsyncSession
+from telebot.asyncio_filters import SimpleCustomFilter
 from telebot.types import Message
 
 from botel.dal import is_commentable
+from botel.utils.common import method_injector
 
 
 async def commentable_filter(db: AsyncSession, message: Message) -> bool:
@@ -10,3 +14,20 @@ async def commentable_filter(db: AsyncSession, message: Message) -> bool:
         group_id=message.chat.id,
         message_id=message.reply_to_message.forward_from_message_id,
     )
+
+
+class IsCommentingFilter(SimpleCustomFilter):
+    """
+    Checking if a message is considered to be part of commenting feature
+    """
+
+    def __init__(
+        self, db_initializer: Callable[[], AsyncGenerator[AsyncSession, None]]
+    ):
+        self.db_initializer = db_initializer
+
+    key = "is_commentable"
+
+    @method_injector("db_initializer")
+    async def check(self, db: AsyncSession, message: Message):
+        return await commentable_filter(db=db, message=message)
